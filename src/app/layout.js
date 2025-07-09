f23c35d1157c19e1f581ca821c5009389585d09d
shishirdubey24 <shishirdubey221@gmail.com>
2025-07-09 18:49:42 +0530

    Updated Imports

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "./(root)/Components/Footer";
+import Footer from "./root/Components/Footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
